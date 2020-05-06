cf8ee79f8a70a30d29ddf9b9bf62819ea199e87d
GiggleLiu <cacate0129@gmail.com>
2020-05-06 04:51:35 -0400

    fasterererer....

--- src/reversible/instructs.jl
+++ src/reversible/instructs.jl
@@ -76,5 +76,52 @@ end
     end
 end
 
+@i function loop_kernel(state::StridedVector{T}, configs, U::MMatrix{2,2}, locs_raw, REG_STACK) where T<:Tropical
+    @invcheckoff begin
+		a ← one(T)
+		b ← one(T)
+		c ← one(T)
+		d ← one(T)
+		@routine @inbounds begin
+			muleq(a, U[1])
+			muleq(b, U[2])
+			muleq(c, U[3])
+			muleq(d, U[4])
+		end
+		incstack!(REG_STACK)
+        @inbounds for i=1:length(configs)
+        	x ← configs[i]
+			is1 ← x + locs_raw[1]
+			is2 ← x + locs_raw[2]
+			s1 ← one(T)
+			s2 ← one(T)
+			NiLang.SWAP(s1, state[is1])
+			NiLang.SWAP(s2, state[is2])
+			@routine begin
+				muleq(a, s1)
+				muleq(c, s2)
+				muleq(b, s1)
+				muleq(d, s2)
+				bk1 ← a.n > c.n
+				bk2 ← b.n > d.n
+			end
+			if (bk1, ~)
+				muleq(state[is1], a)
+			else
+				muleq(state[is1], c)
+			end
+			if (bk2, ~)
+				muleq(state[is2], b)
+			else
+				muleq(state[is2], d)
+			end
+			~@routine
+        	NiLang.SWAP(s1, REG_STACK[is1])
+        	NiLang.SWAP(s2, REG_STACK[is2])
+    	end
+		~@routine
+	end
+end
+
 NiLang.AD.GVar(x::ArrayReg{B}) where B = ArrayReg{B}(GVar(x.state))
 NiLang.AD.GVar(x::VecStack) where B = VecStack(GVar(x.data), x.top)
