9dc8f66abaaf49522773a7db4197ab3dd4f00bfe
GiggleLiu <cacate0129@gmail.com>
2020-05-14 22:14:29 -0400

    correct manefest

--- src/cuda.jl
+++ src/cuda.jl
@@ -1,5 +1,5 @@
 using .CuYao: CuArrays
-using CuArrays: CuArray
+using .CuArrays: CuArray
 
 CuArrays.ones(::Type{Tropical{T}}, dims...) where T = fill!(CuArray{Tropical{T}}(undef, dims...), one(Tropical{T}))
 CuArrays.zeros(::Type{Tropical{T}}, dims...) where T = fill!(CuArray{Tropical{T}}(undef, dims...), zero(Tropical{T}))
