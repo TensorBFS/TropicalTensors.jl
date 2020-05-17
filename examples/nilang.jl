52a8ea1ee8cd70c2d04f156d71c3837bba083e06
GiggleLiu <cacate0129@gmail.com>
2020-05-18 05:55:06 +0800

    new data

--- examples/nilang.jl
+++ examples/nilang.jl
@@ -8,7 +8,8 @@ Js = rand([-1, 1], 2L*(L-1))
 hs = ones(Int, L^2)
 @time solve(Spinglass(SquareLattice(L, L), Js, hs))
 
-L = 22
-Js = rand([-1, 1], 2L*(L-1))
-hs = ones(Int, L^2)
-@time gs = opt_config(Spinglass(SquareLattice(L, L), Js, hs))
+L = 7
+lt = ChimeraLattice(L, L)
+Js = rand(Int16[-1, 1], length(sgbonds(lt)))
+hs = ones(Int16, length(lt))
+tofile("chimera77.dat", @time gs = opt_config(Spinglass(lt, Js, hs)))
