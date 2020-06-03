faed8192bee89ddee2deb9771e164e30941fb765
GiggleLiu <cacate0129@gmail.com>
2020-06-03 20:45:13 +0000

    chimera data fixed

--- project/yao/count_degeneracy.jl
+++ project/yao/count_degeneracy.jl
@@ -1,26 +1,37 @@
 using TropicalTensors
 using DelimitedFiles
+using Suppressor
 using CuYao, CuArrays, CUDAnative
-CUDAnative.device!(1)
+CUDAnative.device!(parse(Int, ARGS[1]))
 
-function random_chimera_degeneracy(Lx::Int, Ly::Int; usecuda)
-    lt = ChimeraLattice(Lx, Ly)
-    sg = Spinglass(lt, rand(Int32[-1,1],length(sgbonds(lt))), zeros(Int32, length(lt)))
-    res = solve(CountingTropical{Int32}, sg; usecuda=usecuda)
-    return res.c
+function random_degeneracy(::Type{T}, lt; usecuda) where T
+    sg = Spinglass(lt, rand(T[-1,1],length(sgbonds(lt))), zeros(T, length(lt)))
+    res = solve(CountingTropical{T}, sg; usecuda=usecuda)
+    return res.n, res.c
 end
 
-function main(Llist, nrepeat::Int; usecuda=false)
-    for (i, L) in enumerate(Llist)
-        out = zeros(Int, nrepeat)
-        for j = 1:nrepeat
-            println("L=$L, $j")
-            @time out[j] = random_chimera_degeneracy(L, L; usecuda=usecuda)
+function main(::Type{T}, ::Type{LT}, Llist, nrepeat::Int; usecuda=false) where {T, LT}
+    @suppress_err begin
+        for (i, L) in enumerate(Llist)
+            lt = LT(L, L)
+            out = zeros(T, nrepeat, 2)
+            for j = 1:nrepeat
+                println("L=$L, $j")
+                @time out[j,:] .= random_degeneracy(T, lt; usecuda=usecuda)
+            end
+            writedlm(joinpath(@__DIR__, "data", "$(name(LT))_degeneracy_L$L.dat"), out)
         end
-        writedlm(joinpath(@__DIR__, "data", "chimera_degeneracy_L$L.dat"), out)
     end
 end
 
+name(::Type{<:ChimeraLattice}) = "chimera"
+name(::Type{<:SquareLattice}) = "square"
+
 if abspath(PROGRAM_FILE) == @__FILE__
-    main(6, 1000; usecuda=true)
+    #main(Int32, ChimeraLattice, 2:4, 1000; usecuda=false)
+    #main(Int32, ChimeraLattice, 5:7, 1000; usecuda=true)
+    #main(Int32, SquareLattice, 8:4:16, 1000; usecuda=false)
+    main(Float64, SquareLattice, parse(Int, ARGS[2]), 1000; usecuda=true)
+    #main(Int64, SquareLattice, [16, 18, 20], 1000; usecuda=true)
+    #main(Int64, SquareLattice, [12 14], 1000; usecuda=false)
 end
