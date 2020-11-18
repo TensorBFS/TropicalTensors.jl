46eb4600e4980331f2a33a02f8e3644c4aa3cafc
GiggleLiu <cacate0129@gmail.com>
2020-11-18 16:55:10 -0500

    fix nnb

--- src/second_neighbor.jl
+++ src/second_neighbor.jl
@@ -10,8 +10,10 @@ Base.size(lt::MaskedSquareLattice) = size(lt.mask)
 Base.size(lt::MaskedSquareLattice, i::Int) = size(lt.mask, i)
 Viznet.unit(lt::MaskedSquareLattice) = 1/(max(size(lt)...))
 Viznet.vertices(lt::MaskedSquareLattice) = [i for i in 1:length(lt.mask) if lt.mask[i]]
-function rand_maskedsquare(Nx::Int, Ny::Int, ρ::Real; seed=2)
-    Random.seed!(seed)
+function rand_maskedsquare(Nx::Int, Ny::Int, ρ::Real; seed=nothing)
+    if seed !== nothing
+        Random.seed!(seed)
+    end
     MaskedSquareLattice(rand(Nx, Ny) .< ρ)
 end
 function Base.getindex(lt::MaskedSquareLattice, i::Real, j::Real)
